a7507155756d27352a62ca0491c0695b1feb8c37
gnebbia <16106651+gnebbia@users.noreply.github.com>
2020-09-20 00:58:33 +0200

    dwadwadawd  dwa dwa

--- .travis/build_binaries.sh
+++ .travis/build_binaries.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 if [ $TRAVIS_OS_NAME = 'osx' ]; then
-  pyinstaller --onefile --paths kb/ kb/__main__.py -n kb_"$TRAVIS_TAG"_osx
+  pyinstaller --onefile --paths kb/ kb/__main__.py -n kb_$TRAVIS_TAG_osx
 elif [ $TRAVIS_OS_NAME = 'windows' ]; then
-  pyinstaller --onefile --paths kb/ kb/__main__.py -n kb_"$TRAVIS_TAG"_win
+  pyinstaller --onefile --paths kb/ kb/__main__.py -n kb_$TRAVIS_TAG_win
 else
-  pyinstaller --onefile --paths kb/ kb/__main__.py -n kb_"$TRAVIS_TAG"_linux
+  pyinstaller --onefile --paths kb/ kb/__main__.py -n kb_$TRAVIS_TAG_linux
 fi
 
