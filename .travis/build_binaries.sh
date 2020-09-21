b66fb7cc4772d5be077c356e74ce4b77d49b0dc9
gnebbia <16106651+gnebbia@users.noreply.github.com>
2020-09-21 23:24:44 +0200

    ALLLAALALLA

--- .travis/build_binaries.sh
+++ .travis/build_binaries.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-RELEASE_VER=$(tr -d "v." <<<"$VERSION_TAG")
+RELEASE_VER=$(tr -d "v." <<<"$TRAVIS_TAG")
 if [ $TRAVIS_OS_NAME = 'osx' ]; then
   pyinstaller --paths kb/ --onefile kb/__main__.py -n kb_${RELEASE_VER}_osx
 elif [ $TRAVIS_OS_NAME = 'windows' ]; then
